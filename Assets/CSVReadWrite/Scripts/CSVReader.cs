08028ac3a7c8dcb4a8dd093c92bd5333600c611b
Shane Martin <shanemartin@shanes-mbp.sae.au>
2015-12-02 19:18:31 +1000

    Updated Singleton to include a better singleton

--- Assets/CSVReadWrite/Scripts/CSVReader.cs
+++ Assets/CSVReadWrite/Scripts/CSVReader.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 
-public class CSVReader : SingletonBehaviour<CSVReader>
+public class CSVReader : SingletonBehaviourII<CSVReader>
 {
 
 	//This is the file we are writing to/reading from
--- Assets/Singletons/Scripts/SingletonBehaviour.cs
+++ Assets/Singletons/Scripts/SingletonBehaviour.cs
@@ -1,34 +1,58 @@
 ﻿using UnityEngine;
-using System.Collections;
 
+/// <summary>
+/// Be aware this will not prevent a non singleton constructor
+///   such as `T myT = new T();`
+/// To prevent that, add `protected T () {}` to your singleton class.
+/// 
+/// As a note, this is made as MonoBehaviour because we need Coroutines.
+/// </summary>
 public class SingletonBehaviour<T> : MonoBehaviour 
-		where T : MonoBehaviour
+	where T : MonoBehaviour
 {
-		
-	protected virtual void OnSingletonAwake ()
-	{
+	public static bool IsPersistent = false;    
+	
+	private static T _instance;
+	private static bool _applicationIsQuitting = false;    
+	private static object _lock = new object ();
+	
+	public static T Instance {
+		get {
+			if (_applicationIsQuitting)
+				return null;
 			
-	}
-	protected virtual void OnSingletonDestroy ()
-	{
-			
-	}
-	public static T Instance { get; private set; }
-	void Awake ()
-	{
-		if (Instance == null) {
-			Instance = this as T;
-		} else {
-			Destroy (gameObject);
+			lock (_lock) {
+				if (_instance == null) {
+					_instance = FindObjectOfType (typeof(T)) as T;
+					
+					if (FindObjectsOfType (typeof(T)).Length > 1)
+						return _instance;
+					
+					if (_instance == null) {
+						GameObject singleton = new GameObject ();
+						_instance = singleton.AddComponent<T> ();
+						singleton.name = "[SINGLETON] " + typeof(T).ToString ();
+						
+						if (IsPersistent)
+							DontDestroyOnLoad (singleton);
+					}
+				}
+				
+				return _instance;
+			}
 		}
-		OnSingletonAwake ();
 	}
-		
-	void OnDestroy ()
+	
+	/// <summary>
+	/// When Unity quits, it destroys objects in a random order.
+	/// In principle, a Singleton is only destroyed when application quits.
+	/// If any script calls Instance after it have been destroyed, 
+	///   it will create a buggy ghost object that will stay on the Editor scene
+	///   even after stopping playing the Application. Really bad!
+	/// So, this was made to be sure we're not creating that buggy ghost object.
+	/// </summary>
+	public void OnDestroy ()
 	{
-		if (Instance == this) {
-			Instance = null;
-		}
-		OnSingletonDestroy ();
+		_applicationIsQuitting = true;
 	}
 }
